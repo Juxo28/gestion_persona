46988e5f04e787a2bf96c1196b670347c16193c7
Juxo28 <julianpalaciol2804@gmail.com>
2026-07-02 14:49:11 -0500

    creacion de persona

--- class/persona.php
+++ class/persona.php
@@ -5,9 +5,10 @@ class persona
     public $nombre;
     public $edad;
     public $correo;
+    public $apellido;
 
     public function Saludar()
         {
-        echo "Hola mi nombre es: ";
+        echo "Hola mi nombre es: ".$this->nombre. " " .$this->apellido;
     }
 }
